f76c5422dc1a617778a6f28072618c406bc10458
yuka <yuka@localhost>
2026-04-30 18:47:22 +0900

    業務終了。TOPページのHTML、CSSから続行する。

--- samplePJ-TODO/src/main/java/sample/SamplePjTodoApplication.java
+++ samplePJ-TODO/src/main/java/sample/SamplePjTodoApplication.java
@@ -1,9 +1,11 @@
-package sample.common.dao.entity;
+package sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("sample.common.dao.mapper")
 public class SamplePjTodoApplication {
 
 	public static void main(String[] args) {
---
+++ samplePJ-TODO/src/main/java/sample/thymeleaf/web/HomePageController.java
@@ -0,0 +1,13 @@
+package sample.thymeleaf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController{
+
+	@GetMapping("/")
+	public String index() {
+		return "homePage";
+}
+}
